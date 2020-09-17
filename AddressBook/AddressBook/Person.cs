9de6309663412ccfd9b23643cd5c381502071bae
ahsan133 <mahsanullah512@gmail.com>
2020-09-17 11:49:28 +0530

    [MD Ahsanullah] Add: person class is added

---
+++ AddressBook/AddressBook/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string city;
+        private string state;
+        private long zip;
+        private long phoneNumber;
+
+        public string FirstName { get => this.firstName; set => this.firstName = value; }
+        public string LastName { get => this.lastName; set => this.lastName = value; }
+        public string Address { get => this.address; set => this.address = value; }
+        public string City { get => this.city; set => this.city = value; }
+        public string State { get => this.state; set => this.state = value; }
+        public long Zip { get => this.zip; set => this.zip = value; }
+        public long PhoneNumber { get => this.phoneNumber; set => this.phoneNumber = value; }
+    }
+}
